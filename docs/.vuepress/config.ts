e0929720dc6247a72905e907aa5a868b53628ac0
Masanori Tani <metro@uta8a.org>
2022-05-30 09:27:38 +0900

    feat: add top page

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,47 @@
+const { defaultTheme } = require('@vuepress/theme-default')
+
+module.exports = {
+  theme: defaultTheme({
+    lang: 'ja',
+    title: 'HiCoder web docs',
+    description: '広島大学コンピュータサークルHiCoderの公開用web資料です。',
+    head: [['link', {rel: 'icon', href: '/images/hicoder.ico'}]],
+    navbar: [
+      // Navbar: 右上に表示される
+      {
+        text: 'はじめに',
+        link: '/guide/',
+      },
+      {
+        text: '座学',
+        link: '/lectures/',
+      },
+      {
+        text: '実習',
+        link: '/practical/',
+      },
+    ],
+    sidebar: {
+      // sidebar: あるパス以下で左に表示されるやつ
+      '/guide/': [
+        {
+          text: "はじめに",
+          children: ['/guide/README.md']
+        }
+      ],
+      '/lectures/': [
+        {
+          text: '座学編',
+          children: ['/lectures/README.md']
+        }
+      ],
+      '/practical/': [
+        {
+          text: '実習編',
+          children: ['/practical/README.md']
+        }
+      ],
+    },
+    logo: '/images/hicoder.png',
+  }),
+}
---
+++ docs/README.md
@@ -0,0 +1,20 @@
+---
+home: true
+heroImage: /images/hicoder.png
+heroText: HiCoder Web講習会資料
+tagline: 
+features:
+- title: はじめに
+  details: Webサービスを作るにあたっての基礎概念と、環境構築について学びます
+- title: 座学編
+  details: Webサービスを作る上で必要な知識を学びます
+- title: 実習編
+  details: 実際に手を動かしてWebサービスを作ります
+footer: MIT Licensed | Copyright © 2022-present HiCoder
+---
+
+右上のナビバーから、もしくは左上のメニューから [**はじめに**](guide/README.md) へ進んでください。
+
+右上の月・太陽アイコンからライトモード/ダークモード切り替えができます。
+
+この資料を読んで、もし**Webに興味が出てきたらぜひHiCoderへ！**
